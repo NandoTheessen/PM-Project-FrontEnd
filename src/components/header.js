18b7e9ceddd4036f80d000330522163bacca7001
FireInjun <Fireinjun291@gmail.com>
2018-11-05 11:20:28 -0500

    edited eslint config

--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,25 @@
-// INIT
+import React from "react";
+import SearchForm from "./SearchForm";
+import "./Header.css";
+const Header = () => (
+  <div>
+    <div className="Headbar">
+      <div>
+        <a href="/all-products">All Products</a>
+      </div>
+      <div>
+        <h1 href="/">Lambda Services</h1>
+      </div>
+      <div>
+        <SearchForm />
+      </div>
+      <div>
+        <a href="/login">Login</a>
+      </div>
+      <div>
+        <a href="/cart">Cart</a>
+      </div>
+    </div>
+  </div>
+);
+export default Header;
