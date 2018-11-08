592b359e3d1c5a4099bb12ff0c489e882f158e18
NandoTheessen <nando.theessen@gmail.com>
2018-11-09 00:06:33 +0100

    Add styled-components as a dependency - Add key to listItems in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './components/jumbotron.css';
+import "./components/jumbotron.css";
 
-import Jumbotron from './components/Jumbotron.js';
-import Minitron from './components/Minitron.js';
+import Jumbotron from "./components/Jumbotron.js";
+import Minitron from "./components/Minitron.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div style={{ height: '75px', width: '100%', backgroundColor: 'white', position: 'relative', }}></div>
-        <Jumbotron styling="jumbotron railroad" description="Designed by experts in the field" title="IT Solutions" button="GET SOLUTION" />
+        <div
+          style={{ height: "75px", width: "100%", backgroundColor: "white", position: "relative" }}
+        />
+        <Jumbotron
+          styling="jumbotron railroad"
+          description="Designed by experts in the field"
+          title="IT Solutions"
+          button="GET SOLUTION"
+        />
         <Minitron smallHeader="Services we provide" listItems={listItems} layout="minitron" />
-        <Jumbotron styling="jumbotron forest" heading="QUALITY IN EVERYTHING WE DO" button="SHOP IT SOLUTIONS" />
+        <Jumbotron
+          styling="jumbotron forest"
+          heading="QUALITY IN EVERYTHING WE DO"
+          button="SHOP IT SOLUTIONS"
+        />
         <Minitron header="POPULAR Services" layout="minitron" />
-        <Jumbotron styling="jumbotron concrete" description="Meet our awesome team members:" title="OUR TEAM" button="READ MORE" />
+        <Jumbotron
+          styling="jumbotron concrete"
+          description="Meet our awesome team members:"
+          title="OUR TEAM"
+          button="READ MORE"
+        />
       </div>
     );
   }
 }
 
 const elements = ["UI/UX", "DATABASE DESIGN", "MOBILE APPS", "BLOGS"];
-const listItems = elements.map((element) => 
-    <li>{element}</li>
-)
+const listItems = elements.map(element => <li key={element}>{element}</li>);
 
 export default App;
